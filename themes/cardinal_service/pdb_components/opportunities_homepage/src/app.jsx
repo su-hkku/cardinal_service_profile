3dba8c223481f68f20f15c3cf109bc2bd2802306
Rebecca Hong <Rebeccahongsf@gmail.com>
2020-06-02 11:25:59 -0700

    CSD-9 Resolved bottom two filter automcomplete flex issue; Update specificity of divs

--- themes/cardinal_service/pdb_components/opportunities_homepage/src/app.jsx
+++ themes/cardinal_service/pdb_components/opportunities_homepage/src/app.jsx
@@ -5,12 +5,12 @@ import './styles.scss';
 
 const nodeBundle = 'su_opportunity';
 const nodeFields = [
-  { field: 'su_opp_type', label: 'Type of Opportunity', multiple: false },
-  { field: 'su_opp_open_to', label: 'Open To', multiple: false },
-  { field: 'su_opp_service_theme', label: 'Service Theme', multiple: false },
-  { field: 'su_opp_time_year', label: 'When', multiple: false },
-  { field: 'su_opp_location', label: 'Location', multiple: false },
-  { field: 'su_opp_dimension', label: 'Dimension', multiple: false },
+  { field: 'su_opp_type', label: 'Type of Opportunity', multiple: true },
+  { field: 'su_opp_open_to', label: 'Open To', multiple: true },
+  { field: 'su_opp_service_theme', label: 'Service Theme', multiple: true },
+  { field: 'su_opp_time_year', label: 'When', multiple: true },
+  { field: 'su_opp_location', label: 'Location', multiple: true },
+  { field: 'su_opp_dimension', label: 'Dimension', multiple: true },
 ];
 
 const blockUuid = Object.keys(drupalSettings.pdb.configuration).find(
