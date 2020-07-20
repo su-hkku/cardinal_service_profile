3a2a570b5803990d4fdf784b5f17ecd6614bd8f8
Mike Decker <pookmish@gmail.com>
2020-07-20 15:55:26 -0700

    added context field and removed other module

--- cardinal_service_profile.post_update.php
+++ cardinal_service_profile.post_update.php
@@ -19,6 +19,39 @@ function cardinal_service_profile_removed_post_updates() {
  * Move field data for spotlights.
  */
 function cardinal_service_profile_post_update_spotlight() {
+  _cardinal_service_create_quote_field();
+  _cardinal_service_create_context_field();
+
+  $nodes = \Drupal::entityTypeManager()
+    ->getStorage('node')
+    ->loadByProperties(['type' => 'su_spotlight']);
+
+  /** @var \Drupal\node\NodeInterface $node */
+  foreach ($nodes as $node) {
+    if ($body = $node->get('body')->getValue()) {
+
+      $quote = [
+        'value' => '<p>' . $body[0]['summary'] . '</p>',
+        'format' => $body[0]['format'],
+      ];
+
+      $student_name = $node->get('su_spotlight_student_name')->getString();
+      if ($student_name == $node->label()) {
+        $node->set('su_spotlight_student_name', NULL);
+      }
+
+      if (!empty($body[0]['value'])) {
+        $node->set('su_spotlight_context', strip_tags($body[0]['value']));
+      }
+
+      $node->set('su_spotlight_quote', $quote)
+        ->set('body', NULL)
+        ->save();
+    }
+  }
+}
+
+function _cardinal_service_create_quote_field() {
   FieldStorageConfig::create([
     'uuid' => '5e126799-c3f1-4b09-bb97-006e8ac0aad2',
     'entity_type' => 'node',
@@ -33,27 +66,20 @@ function cardinal_service_profile_post_update_spotlight() {
     'bundle' => 'su_spotlight',
     'settings' => ['display_summary' => TRUE],
   ])->save();
+}
 
-  $nodes = \Drupal::entityTypeManager()
-    ->getStorage('node')
-    ->loadByProperties(['type' => 'su_spotlight']);
-
-  /** @var \Drupal\node\NodeInterface $node */
-  foreach ($nodes as $node) {
-    if ($body = $node->get('body')->getValue()) {
-      $quote = [
-        'value' => '<p>' . $body[0]['summary'] . '</p>',
-        'summary' => strip_tags($body[0]['value']),
-        'format' => $body[0]['format'],
-      ];
-      $student_name = $node->get('su_spotlight_student_name')->getString();
-      if ($student_name == $node->label()) {
-        $node->set('su_spotlight_student_name', NULL);
-      }
-
-      $node->set('su_spotlight_quote', $quote)
-        ->set('body', NULL)
-        ->save();
-    }
-  }
+function _cardinal_service_create_context_field() {
+  FieldStorageConfig::create([
+    'uuid' => '7bf4acd8-9984-4dc0-b544-112575255a5b',
+    'entity_type' => 'node',
+    'type' => 'string_long',
+    'field_name' => 'su_spotlight_context',
+  ])->save();
+  FieldConfig::create([
+    'uuid' => '2a38e48b-2ea4-4ffd-96b5-ffd40667fe45',
+    'field_name' => 'su_spotlight_context',
+    'label' => 'Caption/Context',
+    'entity_type' => 'node',
+    'bundle' => 'su_spotlight',
+  ])->save();
 }
