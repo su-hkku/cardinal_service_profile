40e3e071333198ff123c7b4e6f8762783dde8b36
Mike Decker <pookmish@gmail.com>
2020-07-22 15:00:02 -0700

    restored file just for upgrade

---
+++ modules/cardinal_service_rest/src/Plugin/rest/resource/OpportunitiesResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\cardinal_service_rest\Plugin\rest\resource;
+
+use Drupal\rest\Plugin\ResourceBase;
+
+/**
+ * Remove this after the next release.
+ *
+ * @RestResource(
+ *   id = "opportunities_resource",
+ *   label = @Translation("Opportunities Resource"),
+ *   uri_paths = {
+ *     "canonical" = "/api/opportunities"
+ *   }
+ * )
+ */
+class OpportunitiesResource extends ResourceBase {
+
+}
