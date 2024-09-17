0b47bb134bc19878c0dd598f21f067a6f6a57533
Mike Decker <pookmish@stanford.edu>
2024-09-17 15:38:12 -0700

    Adjust api endpoint to support parameters

--- themes/cardinal_service/pdb_components/Components/Filters.jsx
+++ themes/cardinal_service/pdb_components/Components/Filters.jsx
@@ -82,6 +82,7 @@ export class Filters extends Component {
 
   constructor(props) {
     super(props);
+    this.props.apiParams = props.apiParams || ""
     this.state = {
       allItems: {},
       activeItems: {},
@@ -113,8 +114,9 @@ export class Filters extends Component {
    */
   componentDidMount() {
     const that = this;
+    const params = this.props.apiParams ? "?" + queryString.stringify(this.props.apiParams, {arrayFormat: 'bracket'}) : ""
 
-    fetch('/api/terms-used/' + this.props.bundle)
+    fetch('/api/terms-used/' + this.props.bundle + params)
       .then((response) => response.json())
       .then((jsonData) => {
         that.setState(
@@ -141,7 +143,6 @@ export class Filters extends Component {
     const query = encodeURI(queryString.stringify(this.state.filters, {arrayFormat: 'bracket'}));
     const location = this.props.submitUrl ?? window.location.pathname;
     window.location = `${location}?${query}#filter-wrapper`;
-
   }
 
   /**
